cdfc2a461798fd563bb34cd32f436eb9fc523d28
gsundell <gsundell8@gmail.com>
2026-02-19 17:24:08 -0600

    Limpia

---
+++ EstructuraDatos/src/main/java/com/mycompany/estructuradatos/EstructuraDatos.java
@@ -0,0 +1,13 @@
+
+
+package com.mycompany.estructuradatos;
+
+public class EstructuraDatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        //Hola 
+    }
+}
