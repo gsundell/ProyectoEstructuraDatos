a0a8b2bb5263cad97ce9548523b3932e1c90d76f
gsundell <gsundell8@gmail.com>
2026-02-19 21:40:04 -0600

    Menu
    Implementacion del menu

--- EstructuraDatos/src/main/java/com/mycompany/estructuradatos/EstructuraDatos.java
+++ EstructuraDatos/src/main/java/com/mycompany/estructuradatos/EstructuraDatos.java
@@ -1,5 +1,3 @@
-
-
 package com.mycompany.estructuradatos;
 
 import javax.swing.JOptionPane;
@@ -7,9 +5,48 @@ import javax.swing.JOptionPane;
 public class EstructuraDatos {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
-        
-        //Hola 
+        Usuarios.Pila usuarios = new Usuarios.Pila();
+        Usuarios.Pila pila = new Usuarios.Pila();
+
+        JOptionPane.showMessageDialog(null, "Para acceder a computadores MYSOFT, debe registrarse primero");
+        pila.registro();
+
+        boolean acceso = false;
+
+        while (!acceso) {
+            JOptionPane.showMessageDialog(null, "Inicie sesión para continuar");
+            acceso = pila.login();
+        }
+
+        String opcion;
+
+        do {
+            opcion = JOptionPane.showInputDialog(
+                    "=== Bienvenido a Computadores MYSOFT ===\n"
+                    + "1.Agregar Usuario\n"
+                    + "2.Inactivar Usuario\n"
+                    + "3.Mostrar Usuarios\n"
+                    + "4.Salir\n ");
+            switch (opcion) {
+
+                case "1":
+                    usuarios.agregar();
+                    break;
+                case "2":
+                    usuarios.inactivarUsuario();
+                    break;
+                case "3":
+                    usuarios.mostrar();
+                    ;
+                    break;
+                case "4":
+                    System.exit(0);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción Inválida");
+
+            }
+
+        } while (!opcion.equals("4"));
     }
 }
